099595e2c2ce5d719416df71ac61f8bff84f1a44
wooz1ewu <apolonik.danila2002@gmail.com>
2021-10-27 13:59:31 +0300

    greetings path logics removed

--- src/modules/greeting/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import greeting from '../controllers/greeting';
-
-const greetingRouter = express.Router();
-
-greetingRouter.get('/', greeting);
-
-export default greetingRouter;
