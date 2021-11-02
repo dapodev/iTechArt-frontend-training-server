86f6c7a19607a11d5caefdd3cee229869d1c3b0b
wooz1ewu <apolonik.danila2002@gmail.com>
2021-11-02 16:39:45 +0300

    authentificateUser.js imports removed, logics clearified

--- training-note-server/src/modules/users/controllers/authentificateUser.js
+++ training-note-server/src/modules/users/controllers/authentificateUser.js
@@ -1,26 +1,14 @@
-import { getUserByEmail } from 'db/providers/users';
-import CommonError from 'errors/CommonError';
-import STATUS_CODES from 'modules/config/constants/statusCodes';
-import { generateMD5fromString } from 'utils/hash';
-
 const authentificateUser = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { userData } = res.locals;
 
   try {
-    //rewrite! but it works!~
-    const user = await getUserByEmail(email);
-    const hashedPassword = generateMD5fromString(password);
+    const user = await userData;
 
-    if (user.password === hashedPassword) {
-      const { email, password, firstName, lastName, birthday } = user;
+    const { email, password, firstName, lastName, birthday } = user;
 
-      res.json({ email, password, firstName, lastName, birthday });
-    } else {
-      throw new CommonError(
-        'Authentification: Password rejected.',
-        STATUS_CODES.clientErrors.INVALID_REQUEST
-      );
-    }
+    const userPayload = { email, password, firstName, lastName, birthday };
+
+    res.json(userPayload);
   } catch (err) {
     next(err);
   }
