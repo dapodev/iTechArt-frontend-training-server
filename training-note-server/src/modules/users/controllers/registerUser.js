eaa234683a1b5e70199520bfd3707116f326e2b5
wooz1ewu <apolonik.danila2002@gmail.com>
2021-11-04 16:13:19 +0300

    users path error logic changed

--- training-note-server/src/modules/users/controllers/registerUser.js
+++ training-note-server/src/modules/users/controllers/registerUser.js
@@ -1,11 +1,10 @@
 import { addUser } from 'db/providers/users';
 
 const registerUser = async (req, res, next) => {
-  try {
-    const { email, firstName, lastName, birthday, password } = req.body;
+  const { email, firstName, lastName, birthday, password } = req.body;
+  const userData = { email, firstName, lastName, birthday, password };
 
-    const userData = { email, firstName, lastName, birthday, password };
-    
+  try {
     const newUser = await addUser(userData);
 
     res.json(newUser);
